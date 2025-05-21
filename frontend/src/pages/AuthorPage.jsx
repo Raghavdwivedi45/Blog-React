2bae28914367b1a98b1e0057e230d0818f9aa51b
Raghav Dwivedi <rjkb143@gmail.com>
2025-05-21 14:27:45 +0530

    Done author page

--- frontend/src/pages/AuthorPage.jsx
+++ frontend/src/pages/AuthorPage.jsx
@@ -1,18 +1,43 @@
-import AuthorCard from '../components/AuthorCard'
-import "../css/AuthorPage.css"
+import AuthorCard from '../components/AuthorCard';
+import "../css/AuthorPage.css";
+import { useRef } from 'react';
 
 
 const AuthorPage = () => {
+
+  const scrollRef = useRef(null);
+
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += 360;
+    }
+  };
+
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft -= 360;
+    }
+  };
+
+
   return (
-        <div className='author-pg-cards'>
-            <img className='author-arrow-left' src="../assets/arrow.png" alt="" />
-            <AuthorCard/>
-            <AuthorCard/>
-            <AuthorCard/>
-            <AuthorCard/>
-            <AuthorCard/>
-            <img className='author-arrow-right' src="../assets/arrow.png" alt="" />
-        </div>
+    <div className='author-container'>
+      <div className="author-arrow-left">
+        <img className='' src="../assets/arrow.png" alt="" onClick={scrollLeft} />
+      </div>
+
+      <div className='author-pg-cards' ref={scrollRef} >
+        <AuthorCard/>
+        <AuthorCard/>
+        <AuthorCard/>
+        <AuthorCard/>
+        <AuthorCard/>
+      </div>
+      
+      <div className="author-arrow-right">
+        <img className='' src="../assets/arrow.png" alt="" onClick={scrollRight} />
+      </div>
+    </div>
   )
 }
 
